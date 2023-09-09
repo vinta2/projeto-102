dfba6a04aea4456aee9174f3334c2f5239e0f055
vinta2 <102837440+vinta2@users.noreply.github.com>
2023-09-09 16:25:16 -0300

    Add files via upload

---
+++ move_file.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+from_dir
+to_dir
+path1
+path2
+path3
+
+list_of_files=os.list_of_files(from_dir)
+#print(list_of_files)
+
+os.path.splitext()
+
+if (True):
+    continue:
+else:
+ lista [ ‘.txt’, ‘.doc’, ‘.docx’, ‘.pdf’’]:
+
+if os.path.exsist(path2):
+    print("movendo"+file_name+".....")
+
+    shutil.move(path1,path3)
+
+else:
+    os.makedirs(path2)
+    print("movendo"+file_name+".....")
+    shutil.move(path1,path3)
